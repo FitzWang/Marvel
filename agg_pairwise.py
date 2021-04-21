24b01f0533bab4fda5b23401be6bc6beb9b0cf16
FitzWang <guangli557@live.com>
2021-04-21 14:41:41 +0200

    new aggragation function: firstly split output to files by suborder, then compute weighted RV and variance

--- agg_pairwise.py
+++ agg_pairwise.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 import time
+import copy
 
 def ExtendOutput(pathbase,csvList):
     for outFile in csvList:
@@ -101,6 +102,80 @@ def RVAggAll2(resultList):
         RV[i] = weightedRV
         Var[i] = weightedVar
     return RV,Var
+
+def SaveDFbySuborder(pathbase,resultList,suborderLen):
+    pathTarget = pathbase / 'bysuborder'
+    if not os.path.exists(pathTarget):
+        os.makedirs(pathTarget)
+    fileLength = len(os.listdir(pathTarget))
+    if fileLength == suborderLen:
+        print('Generated suborder files exist!')
+    else:
+        length = len(resultList)
+        for i in range(suborderLen):
+            outIdx1 = np.empty(length*length,dtype=int)
+            outIdx2 = np.empty(length*length,dtype=int)
+            RV = np.empty(length*length,dtype=float)
+            Var = np.empty(length*length,dtype=float)
+            for idx1 in range(length):
+                for idx2 in range(length):
+                    df = resultList[idx1][idx2]
+                    index = idx1*length + idx2
+                    outIdx1[index] = idx1
+                    outIdx2[index] = idx2
+                    RV[index] = df['RV'].iloc[i]
+                    Var[index] = df['var'].iloc[i]
+            dfOut = pd.DataFrame({'idx1':outIdx1,'idx2':outIdx2,'RV':RV,'var':Var})
+            outFileName = 'suborder_'+str(i)+'.csv'
+            dfOut.to_csv(pathTarget/outFileName,index=False)
+    return pathTarget
+
+def RVSuborder2(pathTarget,N):
+    csvList = os.listdir(pathTarget)
+    csvList = [filename for filename in csvList if filename.endswith('.csv') and filename.startswith('suborder')]
+    csvList.sort()
+    
+    RVList = []
+    for file in csvList:
+        df = pd.read_csv(pathTarget/file)
+        RVoneOrder = np.empty(N,dtype=float)
+        VaroneOrder = np.empty(N,dtype=float)
+        for idx1 in range(N):
+            dfsub = df[df['idx1']==idx1]
+            weight = (1/dfsub['var'])/(1/dfsub['var']).sum()
+            weightedRV = (weight * dfsub['RV']).sum()
+            weightedVar = 1/(1/dfsub['var']).sum()
+            RVoneOrder[idx1] = weightedRV
+            VaroneOrder[idx1] = weightedVar
+        dfoneOrder = pd.DataFrame({'RVl':RVoneOrder,'varl':VaroneOrder})
+        RVList.append(dfoneOrder)
+    return RVList
+
+def RVAggAll3(RVList,N):
+    length = len(RVList)
+    RVout = np.empty(N,dtype=float)
+    Varout = np.empty(N,dtype=float)
+    for idx1 in range(N):
+        RVtmp = np.empty(length,dtype=float)
+        Vartmp = np.empty(length,dtype=float)
+        for i in range(length):
+            RVtmp[i] = RVList[i]['RVl'].iloc[idx1]
+            Vartmp[i] = RVList[i]['varl'].iloc[idx1]
+        weight = 1/Vartmp
+        weightsum = np.sum(weight)
+        weightedRV = np.sum(RVtmp*weight/weightsum)
+        weightedVar = 1/weightsum
+        RVout[idx1] = weightedRV
+        Varout[idx1] = weightedVar
+    return RVout, Varout
+
+def LeaveOneOutCorr(RVList,N,removeIdx):
+    remainingList = copy.deepcopy(RVList)
+    RVselected = remainingList.pop(removeIdx)
+    RVRemaing,_ = RVAggAll3(remainingList,N)
+    corr = np.corrcoef(RVRemaing, RVselected['RVl'])
+    return corr[0,1]
+    
 if __name__ == '__main__':
     #############parameters########
     fileExtend = False  # specify if need to extend output file (e.g. only optimize pairwise 1-0, not 0-1)
@@ -108,6 +183,8 @@ if __name__ == '__main__':
     targetFolder = 'HPC_2021_03_22'
     cutOffOrder = 43
     N = 41
+    ContaminationDet = False
+    bottomK = 20
     #############################
     pathbase = Path(os.getcwd()) / '..' /'results'/ targetFolder
     
@@ -123,14 +200,43 @@ if __name__ == '__main__':
         csvList.sort()
         
     # Read all files into one 2d array    
+    print('Reading all the raw output CSV files......')
+    start = time.perf_counter()
     resultAll = ReadAll(N,pathbase,csvList)
-    lengthOut = len(resultAll[0][0])
-    # Deal with suborders rv
-    RVList = RVSuborder(resultAll,lengthOut)
-    # Final output
-    RVout,Varout = RVAggAll(RVList)
+    print('time:{}s'.format(time.perf_counter()-start))
     
-    # RVout,Varout = RVAggAll2(resultAll)
+    lenSuborder = len(resultAll[0][0])
+    start = time.perf_counter()
+    print('Saving New CSV files by suborders......')
+    pathDFByOrder = SaveDFbySuborder(pathbase,resultAll,lenSuborder)
+    print('time:{}s'.format(time.perf_counter()-start))
+    
+    # Deal with suborders rv
+    start = time.perf_counter()
+    print('Computing RV estimation for all suborders......')
+    RVListbySuborder = RVSuborder2(pathDFByOrder,N)
+    # Final output
+    RVout,Varout = RVAggAll3(RVListbySuborder,N)
+    print('time:{}s'.format(time.perf_counter()-start))
+    
+    # compute correlation between using leave-one-out method
+    if ContaminationDet:
+        start = time.perf_counter()
+        print('Computing correlation by leave-one-out method......')
+        corr = map(lambda i:LeaveOneOutCorr(RVListbySuborder,N,i),range(lenSuborder))
+        corr = list(corr)
+        print('time:{}s'.format(time.perf_counter()-start))
+        RemaingCorrIdx = np.array(corr).argsort()[bottomK:]
+        RVListRmSmCorr = [RVListbySuborder[i] for i in RemaingCorrIdx]
+        RVRmLowCorr,VarRmLowCorr = RVAggAll3(RVListRmSmCorr,N)
+        
+    
+    # # Deal with suborders rv
+    # RVList = RVSuborder(resultAll,lengthOut)
+    # # Final output
+    # RVout,Varout = RVAggAll(RVList)
+    
+    # # RVout,Varout = RVAggAll2(resultAll)
     
     # plot the results
     RVTrue = pd.read_csv(pathbase/'RVTrue.txt')['RVTrue'].to_numpy()
@@ -155,8 +261,29 @@ if __name__ == '__main__':
     ax1.set_xlabel("Index of epochs")
     ax1.set_ylabel("Radial Velocity/ m/s (mean-substracted)")
     ax1.grid(True, color='gainsboro', linestyle='-', linewidth=0.5)
+    ax1.set_title('RV estimation using all suborders')
     fig1.legend()
+    
+    if ContaminationDet:
+        fig2, ax2 = plt.subplots(figsize=(11,7))
+        error = 1.96*np.sqrt(VarRmLowCorr)
+        ax2.plot(X,meansubTrueRV,'-',label = "True Radial Velocity (mean-substracted)")
+        turerms1 = np.around(np.sqrt(((RVRmLowCorr-meansubTrueRV)**2).mean()),decimals=2)
+        ax2.errorbar(X, RVRmLowCorr, yerr=error,color='black',capsize=3, fmt='o',label = "Estimated Radial Velocity, RMSE:"+str(turerms1))
+        ax2.set_xlabel("Index of epochs")
+        ax2.set_ylabel("Radial Velocity/ m/s (mean-substracted)")
+        ax2.grid(True, color='gainsboro', linestyle='-', linewidth=0.5)
+        ax2.set_title('RV estimation after removing suborders with low correlation')
+        fig2.legend()
+    
+    # plot the histogram of variance using all suborders
+    fig3, ax3 = plt.subplots(figsize=(11,7))
+    ax3.hist(Varout)
+    ax3.set_title('Histogram of Variance for Each RV Estimation')
     
     if saveImg == True:
         fig.savefig(pathbase / 'graph' / "RVcomparison.png")
         fig1.savefig(pathbase / 'graph' / "RVcomparison1.png")
+        fig3.savefig(pathbase /'graph'/'hist.png')
+        if ContaminationDet:
+            fig2.savefig(pathbase / 'graph' / "RVcomparison2.png")
